40c6e2cbc398600d5ff012194c18e31cf6d65be9
Aïssetou SACKO <aissetou.sck@gmail.com>
2023-11-08 21:58:06 +0100

    Adding empty modal

--- app/learn-home/page.jsx
+++ app/learn-home/page.jsx
@@ -1,10 +1,13 @@
+'use client'
 import styles from 'styles/pages/LearnHome.module.scss'
 import stylesTag from '../components/Tags/Tags.module.scss'
 import lhDisplay from 'public/learn_home/lh_display.png'
 // import Tags from '../components/Tags/Tags'
-// import { FaImage } from 'react-icons/fa'
+import { FaImage } from 'react-icons/fa'
 // import { FaTimes } from 'react-icons/fa'
 import Image from 'next/image'
+import Modal from '../components/Modal/Modal'
+import { Fragment, useState } from 'react'
 
 const LearnHome = () => {
   /**
@@ -22,63 +25,60 @@ const LearnHome = () => {
   //   modalContainer.classList.toggle('active')
   // }
 
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <div className={styles.learn}>
-      <h1>Learn@Home</h1>
+    <Fragment>
+      <div className={styles.learn}>
+        <h1>Learn@Home</h1>
 
-      <div className={styles.learn__infos}>
-        <Image
-          src={lhDisplay}
-          width={750}
-          height={560}
-          alt="Mockup Learn@Home"
-        />
+        <div className={styles.learn__infos}>
+          <Image
+            src={lhDisplay}
+            width={750}
+            height={560}
+            alt="Mockup Learn@Home"
+          />
 
-        <div className={styles.learn__infosRight}>
-          <p className={styles.learn__description}>
-            Learn@Home est une application de soutien scolaire permettant à un
-            bénévole et un élève de communiquer entre eux. L&apos;objectif de ce
-            projet a été de concevoir le design du site web et de le rendre
-            accessible sur un ordinateur ou un smartphone.
-          </p>
+          <div className={styles.learn__infosRight}>
+            <p className={styles.learn__description}>
+              Learn@Home est une application de soutien scolaire permettant à un
+              bénévole et un élève de communiquer entre eux. L&apos;objectif de
+              ce projet a été de concevoir le design du site web et de le rendre
+              accessible sur un ordinateur ou un smartphone.
+            </p>
 
-          {/* <Tags text="Figma" tag="tech" /> */}
-          <p className={stylesTag.tech}>Figma</p>
+            {/* <Tags text="Figma" tag="tech" /> */}
+            <p className={stylesTag.tech}>Figma</p>
 
-          <div className={styles.learn__infosPart2}>
-            <div className={styles.learn__date}>
-              <h1>Date</h1>
-              <p>2022</p>
+            <div className={styles.learn__infosPart2}>
+              <div className={styles.learn__date}>
+                <h1>Date</h1>
+                <p>2022</p>
+              </div>
+
+              <div className={styles.learn__type}>
+                <h1>Type de projet</h1>
+                <p>Projet de formation</p>
+              </div>
             </div>
 
-            <div className={styles.learn__type}>
-              <h1>Type de projet</h1>
-              <p>Projet de formation</p>
-            </div>
+            {/* <Tags text="UI Design" tag="tag-ui" /> */}
+            <p className={stylesTag.tagUi}>UI Design</p>
           </div>
-
-          {/* <Tags text="UI Design" tag="tag-ui" /> */}
-          <p className={stylesTag.tagUi}>UI Design</p>
         </div>
+
+        {/* Bouton pour ouvrir la modale */}
+        <button
+          className={styles.modal__btn + ' ' + styles.modal__trigger}
+          onClick={() => setShowModal(true)}
+        >
+          <FaImage />
+        </button>
+
+        <Modal isVisible={showModal} onClose={() => setShowModal(false)} />
       </div>
-
-      {/* <div className="modal__container">
-        <div className="overlay modal__trigger"></div>
-
-        <div className="modal">
-          <button className="modal__close">
-            <FaTimes />
-          </button>
-
-          <h1>Soon...</h1>
-        </div>
-      </div> */}
-
-      {/* Bouton pour ouvrir la modale */}
-      {/* <button className="modal__btn modal__trigger">
-        <FaImage />
-      </button> */}
-    </div>
+    </Fragment>
   )
 }
 
