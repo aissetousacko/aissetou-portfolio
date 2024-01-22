65b80afa26d4f0982c7802cd9807c791a9454e03
Aïssetou SACKO <aissetou.sck@gmail.com>
2024-01-22 21:44:50 +0100

    Adding hamburger menu

--- src/app/projets/page.jsx
+++ src/app/projets/page.jsx
@@ -8,31 +8,34 @@ import learnHome from 'public/learn_home/lh_cover.png'
 
 const Projets = () => {
   return (
-    <div className="home__work">
-      <Link href="/kda">
-        <Card img={kda} title="K/DA" description="Site de présentation" />
-      </Link>
-      <Link href="/oxyty">
-        <Card
-          img={oxyty}
-          title="Oxyty"
-          description="Redesign de la page d'accueil"
-        />
-      </Link>
-      <Link href="/hrnet">
-        <Card
-          img={hrnet}
-          title="HRNet"
-          description="Site de gestion d'employés"
-        />
-      </Link>
-      <Link href="/learn-home">
-        <Card
-          img={learnHome}
-          title="Learn@Home"
-          description="Application de soutien scolaire"
-        />
-      </Link>
+    <div className="projets">
+      <h1>Mes projets</h1>
+      <div className="projets__work">
+        <Link href="/kda">
+          <Card img={kda} title="K/DA" description="Site de présentation" />
+        </Link>
+        <Link href="/oxyty">
+          <Card
+            img={oxyty}
+            title="Oxyty"
+            description="Redesign de la page d'accueil"
+          />
+        </Link>
+        <Link href="/hrnet">
+          <Card
+            img={hrnet}
+            title="HRNet"
+            description="Site de gestion d'employés"
+          />
+        </Link>
+        <Link href="/learn-home">
+          <Card
+            img={learnHome}
+            title="Learn@Home"
+            description="Application de soutien scolaire"
+          />
+        </Link>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,54 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from 'public/logo.png'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [active, setActive] = useState('navbar__tabs')
+  const [icon, setIcon] = useState('hamburger')
+
+  const navToggle = () => {
+    active === 'navbar__tabs'
+      ? setActive('navbar__tabs active')
+      : setActive('navbar__tabs')
+
+    icon === 'hamburger' ? setIcon('hamburger active') : setIcon('hamburger')
+  }
+
+  // hamburger.addEventListener('click', () => {
+  //   console.log('click')
+  //   hamburger.classList.toggle('active')
+  //   nav.classList.toggle('active')
+  // })
+
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <Link href="/">
         <Image src={logo} alt="Logo" />
       </Link>
-      <ul className="navbar__tabs">
+      <ul className={active}>
         <li>
-          <Link href="/projets">Projets</Link>
+          <Link href="/projets" className="navbar__item">
+            Projets
+          </Link>
         </li>
         <li>
-          <Link href="/about">À propos</Link>
+          <Link href="/about" className="navbar__item">
+            À propos
+          </Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href="/contact" className="navbar__item">
+            Contact
+          </Link>
         </li>
       </ul>
-    </div>
+      <div className={icon} onClick={navToggle}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
   )
 }
 
