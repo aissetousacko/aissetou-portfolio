8413a584fd323f91bde3785d598dcf13091165d6
Aïssetou SACKO <aissetou.sck@gmail.com>
2024-01-23 11:44:08 +0100

    Adding grid background

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
-import Head from 'next/head'
 import '../styles.scss'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import lhDisplay from 'public/learn_home/lh_display.png'
 
 export const metadata = {
   title: 'Aïssetou SACKO - Portfolio',
@@ -12,13 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
-        <link rel="preload" href={lhDisplay} as="image" />
-      </Head> */}
       <body>
-        <Navbar />
-        {children}
-        <Footer />
+        <main>
+          <Navbar />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   )
